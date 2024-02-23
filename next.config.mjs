ace08c3447f2ab72612e5ccb99ef5e8fa76b2ac3
Ankur Saxena <96392974+ankur724@users.noreply.github.com>
2024-02-24 01:24:06 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
- 
- 
-}
- 
-module.exports = nextConfig
+module.exports = {
+  // Your other configurations...
+  // Specify export behavior using the 'output' property
+  // Use 'export' as the value to generate static HTML exports
+  output: {
+    // Set 'export' as the target
+    // This generates a static HTML export
+    // Make sure to replace 'export' with 'html' if you want to use the static HTML export feature
+    // 'html' will be the new value starting from Next.js 12
+    target: 'export',
+  },
+  // Other configurations...
+};
